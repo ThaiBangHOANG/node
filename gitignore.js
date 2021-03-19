ed9174a5bb086495661db9fe2c82c3eaf138ec11
Thai Bang <thaibang1983hn@gmail.com>
2021-03-19 17:10:16 +0100

    upload file cow say

---
+++ gitignore.js
@@ -0,0 +1,18 @@
+npm WARN questJS No license field.
+
++ cowsay@1.4.0
+added 10 packages from 3 contributors and audited 10 packages in 1.021s
+found 1 low severity vulnerability
+  run `npm audit fix` to fix them, or `npm audit` for details
+thaibang_dev@:~/Desktop/questJS$ node index.js
+{ nom: 'Thai Bang', campus: 'Lille' }
+ _________________
+< I'm a moooodule >
+ -----------------
+        \   ^__^
+         \  (oO)\_______
+            (__)\       )\/\
+             U  ||----w |
+                ||     ||
+thaibang_dev@:~/Desktop/questJS$ 
+
